1de6f6e038cb493e590edab52d460004c6d4c906
Thomas Jaensch <thomasjaensch@gmail.com>
2018-07-24 10:08:18 -0400

    Changed @Controller to @RestController

--- src/main/java/gov/noaa/onestop/gcmd/controller/GcmdController.java
+++ src/main/java/gov/noaa/onestop/gcmd/controller/GcmdController.java
@@ -2,21 +2,18 @@ package gov.noaa.onestop.gcmd.controller;
 
 import gov.noaa.onestop.gcmd.service.GcmdService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class GcmdController {
 
     @Autowired
     GcmdService service;
 
     @RequestMapping("/gcmd")
-    @ResponseBody
     public List<String> gcmdPage(){
         return service.findXmlFiles();
     }

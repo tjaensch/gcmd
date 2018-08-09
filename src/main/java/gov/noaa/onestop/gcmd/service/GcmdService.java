714de4eec4a74b4e73dfdeb3111b7f83ed74f846
Thomas Jaensch <thomasjaensch@gmail.com>
2018-08-09 10:03:51 -0400

    Accounted for dataCenter and dataCentre codeListValue differences

--- src/main/java/gov/noaa/onestop/gcmd/service/GcmdService.java
+++ src/main/java/gov/noaa/onestop/gcmd/service/GcmdService.java
@@ -16,6 +16,7 @@ import javax.xml.xpath.XPathFactory;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -64,6 +65,7 @@ public class GcmdService {
     // DATACENTER KEYWORDS
     public List<String> get_datacenter_keywords(Document xmlDocument) throws IOException, XPathExpressionException {
         XPath xPath = XPathFactory.newInstance().newXPath();
+        // codeListValue 'dataCentre'
         String expression = "//*[local-name()='MD_DataIdentification']/*[local-name()"
                 + "='descriptiveKeywords']/*[local-name()='MD_Keywords'][*[local-name()='type']/*[local-name()"
                 + "='MD_KeywordTypeCode'][@*[local-name() = 'codeListValue' and .='dataCentre']]]/*[local-name()"
@@ -71,10 +73,28 @@ public class GcmdService {
                 + "='title']/*[contains(text(), 'GCMD')]]/*";
         NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
 
-        List<String> datacenterKeywords = IntStream.range(0, nodeList.getLength())
+        List<String> datacenterKeywords1 = IntStream.range(0, nodeList.getLength())
                 .mapToObj(nodeList::item)
                 .map(n -> n.getTextContent().replace("\n", "").trim().replaceAll(" +", " "))
                 .collect(Collectors.toList());
+
+        // codeListValue 'dataCenter'
+        expression = "//*[local-name()='MD_DataIdentification']/*[local-name()"
+                + "='descriptiveKeywords']/*[local-name()='MD_Keywords'][*[local-name()='type']/*[local-name()"
+                + "='MD_KeywordTypeCode'][@*[local-name() = 'codeListValue' and .='dataCenter']]]/*[local-name()"
+                + "='keyword'][../*[local-name()='thesaurusName']/*[local-name()='CI_Citation']/*[local-name()"
+                + "='title']/*[contains(text(), 'GCMD')]]/*";
+        NodeList nodeList2 = (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
+
+        List<String> datacenterKeywords2 = IntStream.range(0, nodeList2.getLength())
+                .mapToObj(nodeList2::item)
+                .map(n -> n.getTextContent().replace("\n", "").trim().replaceAll(" +", " "))
+                .collect(Collectors.toList());
+
+        // join both lists into one (one of them is most likely empty)
+        List<String> datacenterKeywords = new ArrayList<String>();
+        datacenterKeywords.addAll(datacenterKeywords1);
+        datacenterKeywords.addAll(datacenterKeywords2);
 
         return datacenterKeywords;
     }

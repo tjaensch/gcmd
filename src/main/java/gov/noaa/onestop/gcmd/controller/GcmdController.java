7b367fd41141b45fb5bf0d296fb74a33a398aaee
Thomas Jaensch <thomasjaensch@gmail.com>
2018-08-15 14:53:02 -0400

    Made all routes return JSON

--- src/main/java/gov/noaa/onestop/gcmd/controller/GcmdController.java
+++ src/main/java/gov/noaa/onestop/gcmd/controller/GcmdController.java
@@ -28,7 +28,7 @@ public class GcmdController {
 
     // Format http://localhost:8080/gcmd_keywords?url=https://data.nodc.noaa
     // .gov/nodc/archive/metadata/approved/iso/GHRSST-ABOM-L4HRfnd-AUS-RAMSSA_09km.xml
-    @RequestMapping(value = "/gcmd_keywords", method = RequestMethod.GET)
+    @RequestMapping(value = "/gcmd_keywords", method = RequestMethod.GET, produces = "application/json")
     public HashMap<String, ArrayList> get_url_value(@RequestParam("url") URL urlValue) throws IOException, SAXException, XPathExpressionException {
         URL xmlUrl = urlValue;
         xmlDocument = service.get_xml_document(xmlUrl);
@@ -40,119 +40,119 @@ public class GcmdController {
     }
 
     // ALL KEYWORDS
-    @RequestMapping("/show_all_keywords")
+    @RequestMapping(value = "/show_all_keywords", produces = "application/json")
     public HashMap<String, ArrayList> show_all_keywords() throws Exception {
         return service.get_all_keywords();
     }
 
-    @RequestMapping("/show_all_invalid_keywords")
+    @RequestMapping(value = "/show_all_invalid_keywords", produces = "application/json")
     public HashMap<String, ArrayList> show_all_invalid_keywords() throws Exception {
         return service.get_all_invalid_keywords();
     }
 
     // SIMILAR KEYWORDS
-    @RequestMapping("/show_similar_keywords")
+    @RequestMapping(value = "/show_similar_keywords", produces = "application/json")
     public List<String> get_similar_keywords() throws Exception {
         return service.get_similar_keywords(service.get_model_instrument_keywords_list(), "Winds");
     }
 
     // THEME KEYWORDS
-    @RequestMapping("/show_theme_keywords")
+    @RequestMapping(value = "/show_theme_keywords", produces = "application/json")
     public List<String> show_theme_keywords() throws Exception {
         return service.get_theme_keywords(xmlDocument);
     }
 
-    @RequestMapping("/show_model_theme_keywords")
+    @RequestMapping(value = "/show_model_theme_keywords", produces = "application/json")
     public List<String> get_model_theme_keywords_list() throws IOException, SAXException {
         return service.get_model_theme_keywords_list();
     }
 
-    @RequestMapping("/show_invalid_theme_keywords")
+    @RequestMapping(value = "/show_invalid_theme_keywords", produces = "application/json")
     public List<String> show_invalid_theme_keywords() throws IOException, SAXException, XPathExpressionException {
         return service.get_invalid_theme_keywords();
     }
 
     // DATACENTER KEYWORDS
-    @RequestMapping("/show_datacenter_keywords")
+    @RequestMapping(value = "/show_datacenter_keywords", produces = "application/json")
     public List<String> show_datacenter_keywords() throws IOException, XPathExpressionException, ParserConfigurationException,
             SAXException, TransformerException {
         return service.get_datacenter_keywords(xmlDocument);
     }
 
-    @RequestMapping("/show_model_datacenter_keywords")
+    @RequestMapping(value = "/show_model_datacenter_keywords", produces = "application/json")
     public List<String> get_model_datacenter_keywords_list() throws IOException, SAXException {
         return service.get_model_datacenter_keywords_list();
     }
 
-    @RequestMapping("/show_invalid_datacenter_keywords")
+    @RequestMapping(value = "/show_invalid_datacenter_keywords", produces = "application/json")
     public List<String> show_invalid_datacenter_keywords() throws IOException, SAXException, XPathExpressionException {
         return service.get_invalid_datacenter_keywords();
     }
 
     // PLACE KEYWORDS
-    @RequestMapping("/show_place_keywords")
+    @RequestMapping(value = "/show_place_keywords", produces = "application/json")
     public List<String> show_place_keywords() throws IOException, XPathExpressionException, ParserConfigurationException,
             SAXException, TransformerException {
         return service.get_place_keywords(xmlDocument);
     }
 
-    @RequestMapping("/show_model_place_keywords")
+    @RequestMapping(value = "/show_model_place_keywords", produces = "application/json")
     public List<String> get_model_place_keywords_list() throws IOException, SAXException {
         return service.get_model_place_keywords_list();
     }
 
-    @RequestMapping("/show_invalid_place_keywords")
+    @RequestMapping(value = "/show_invalid_place_keywords", produces = "application/json")
     public List<String> show_invalid_place_keywords() throws IOException, SAXException, XPathExpressionException {
         return service.get_invalid_place_keywords();
     }
 
     // PLATFORM KEYWORDS
-    @RequestMapping("/show_platform_keywords")
+    @RequestMapping(value = "/show_platform_keywords", produces = "application/json")
     public List<String> show_platform_keywords() throws IOException, XPathExpressionException, ParserConfigurationException,
             SAXException, TransformerException {
         return service.get_platform_keywords(xmlDocument);
     }
 
-    @RequestMapping("/show_model_platform_keywords")
+    @RequestMapping(value = "/show_model_platform_keywords", produces = "application/json")
     public List<String> get_model_platform_keywords_list() throws IOException, SAXException {
         return service.get_model_platform_keywords_list();
     }
 
-    @RequestMapping("/show_invalid_platform_keywords")
+    @RequestMapping(value = "/show_invalid_platform_keywords", produces = "application/json")
     public List<String> show_invalid_platform_keywords() throws IOException, SAXException, XPathExpressionException {
         return service.get_invalid_platform_keywords();
     }
 
     // INSTRUMENT KEYWORDS
-    @RequestMapping("/show_instrument_keywords")
+    @RequestMapping(value = "/show_instrument_keywords", produces = "application/json")
     public List<String> show_instrument_keywords() throws IOException, XPathExpressionException, ParserConfigurationException,
             SAXException, TransformerException {
         return service.get_instrument_keywords(xmlDocument);
     }
 
-    @RequestMapping("/show_model_instrument_keywords")
+    @RequestMapping(value = "/show_model_instrument_keywords", produces = "application/json")
     public List<String> get_model_instrument_keywords_list() throws IOException, SAXException {
         return service.get_model_instrument_keywords_list();
     }
 
-    @RequestMapping("/show_invalid_instrument_keywords")
+    @RequestMapping(value = "/show_invalid_instrument_keywords", produces = "application/json")
     public List<String> show_invalid_instrument_keywords() throws IOException, SAXException, XPathExpressionException {
         return service.get_invalid_instrument_keywords();
     }
 
     // PROJECT KEYWORDS
-    @RequestMapping("/show_project_keywords")
+    @RequestMapping(value = "/show_project_keywords", produces = "application/json")
     public List<String> show_project_keywords() throws IOException, XPathExpressionException, ParserConfigurationException,
             SAXException, TransformerException {
         return service.get_project_keywords(xmlDocument);
     }
 
-    @RequestMapping("/show_model_project_keywords")
+    @RequestMapping(value = "/show_model_project_keywords", produces = "application/json")
     public List<String> get_model_project_keywords_list() throws IOException, SAXException {
         return service.get_model_project_keywords_list();
     }
 
-    @RequestMapping("/show_invalid_project_keywords")
+    @RequestMapping(value = "/show_invalid_project_keywords", produces = "application/json")
     public List<String> show_invalid_project_keywords() throws IOException, SAXException, XPathExpressionException {
         return service.get_invalid_project_keywords();
     }
